96c891151a384663bff5f93fe6bb2f9d0ba09b9e
Aayu Parikh <aryanparikh03@gmail.com>
2024-12-27 16:39:54 +0530

    Iterator pattern is being implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,64 @@
 package org.example;
 
+// Iterator Interface
+interface Iterator<T> {
+    boolean hasNext(); // Check if there are more elements
+    T next();          // Get the next element
+}
+
+// Aggregate Interface
+interface Collection<T> {
+    Iterator<T> createIterator(); // Return an iterator
+}
+
+// Concrete Iterator
+class BookIterator implements Iterator<String> {
+    private String[] books;
+    private int position = 0;
+
+    public BookIterator(String[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < books.length;
+    }
+
+    @Override
+    public String next() {
+        if (!hasNext()) {
+            throw new IllegalStateException("No more elements");
+        }
+        return books[position++];
+    }
+}
+
+// Concrete Aggregate
+class BookCollection implements Collection<String> {
+    private String[] books;
+
+    public BookCollection(String[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<String> createIterator() {
+        return new BookIterator(books);
+    }
+}
+
+// Client
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] books = {"Design Patterns", "Clean Code", "Refactoring", "Effective Java"};
+
+        BookCollection bookCollection = new BookCollection(books);
+        Iterator<String> iterator = bookCollection.createIterator();
+
+        System.out.println("Book List:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
