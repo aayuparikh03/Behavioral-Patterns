2de1fb5d31a83849ec791914ec90a95ed89207fe
Aayu Parikh <aryanparikh03@gmail.com>
2024-12-30 11:45:52 +0530

    State is implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,89 @@
 package org.example;
 
+// State Interface
+interface TrafficLightState {
+    void changeLight(TrafficLightContext context);
+    String getState();
+}
+
+// Concrete State: Red Light
+class RedLightState implements TrafficLightState {
+    @Override
+    public void changeLight(TrafficLightContext context) {
+        System.out.println("Switching to Green Light...");
+        context.setState(new GreenLightState());
+    }
+
+    @Override
+    public String getState() {
+        return "Red Light";
+    }
+}
+
+// Concrete State: Green Light
+class GreenLightState implements TrafficLightState {
+    @Override
+    public void changeLight(TrafficLightContext context) {
+        System.out.println("Switching to Yellow Light...");
+        context.setState(new YellowLightState());
+    }
+
+    @Override
+    public String getState() {
+        return "Green Light";
+    }
+}
+
+// Concrete State: Yellow Light
+class YellowLightState implements TrafficLightState {
+    @Override
+    public void changeLight(TrafficLightContext context) {
+        System.out.println("Switching to Red Light...");
+        context.setState(new RedLightState());
+    }
+
+    @Override
+    public String getState() {
+        return "Yellow Light";
+    }
+}
+
+// Context: Traffic Light
+class TrafficLightContext {
+    private TrafficLightState currentState;
+
+    public TrafficLightContext() {
+        // Initial state
+        this.currentState = new RedLightState();
+    }
+
+    public void setState(TrafficLightState state) {
+        this.currentState = state;
+    }
+
+    public void changeLight() {
+        currentState.changeLight(this);
+    }
+
+    public String getCurrentState() {
+        return currentState.getState();
+    }
+}
+
+// Client
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TrafficLightContext trafficLight = new TrafficLightContext();
+
+        System.out.println("Initial State: " + trafficLight.getCurrentState());
+
+        trafficLight.changeLight();
+        System.out.println("Current State: " + trafficLight.getCurrentState());
+
+        trafficLight.changeLight();
+        System.out.println("Current State: " + trafficLight.getCurrentState());
+
+        trafficLight.changeLight();
+        System.out.println("Current State: " + trafficLight.getCurrentState());
     }
 }
